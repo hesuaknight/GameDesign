a8f0bf077bcb4991f88eac449dbc5f1388c93ddf
Silvestre Queiruga <sylvergq@gmail.com>
2018-01-03 19:49:11 -0300

    Level
    Estructura base del nivel

---
+++ GameDesign/Assets/Scripts/UpdateManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateManager: MonoBehaviour
+{
+    private List<IUpdate> toUpdate = new List<IUpdate>();
+    public static UpdateManager instance { get { return _instance; } }
+    private static UpdateManager _instance;
+
+    public void Start()
+    {
+        if (_instance == null)
+            _instance = this;
+    }
+
+    public void AddUpdate(IUpdate updateObj)
+    {
+        toUpdate.Add(updateObj);
+    }
+
+    public void RemoveUpdate(IUpdate updateObj)
+    {
+        toUpdate.Remove(updateObj);
+    }
+
+    public void Update()
+    {
+        foreach (var item in toUpdate)
+            item.Update();
+    }
+}
+
+
+public interface IUpdate{
+    void Update();
+   
+}
